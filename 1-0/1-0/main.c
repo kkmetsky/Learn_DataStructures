c6f164f83c2a12f6bcf4cbdb22405455db6b4298
jim <kkmetsky@hotmail.com>
2013-01-22 10:27:36 +0800

    init

---
+++ 1-0/1-0/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  1-0
+//
+//  Created by liu yao-chun on 13/1/22.
+//  Copyright (c) 2013年 liu yao-chun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
