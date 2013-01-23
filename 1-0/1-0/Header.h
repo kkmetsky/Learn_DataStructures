6ec927dfa5cbe61d55f444b6666d35664b01d4b4
jim <kkmetsky@hotmail.com>
2013-01-23 17:29:24 +0800

    add Header.h
    function:
    char string & caulate word

---
+++ 1-0/1-0/Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  1-0
+//
+//  Created by liu yao-chun on 13/1/23.
+//  Copyright (c) 2013年 liu yao-chun. All rights reserved.
+//
+
+#ifndef __0_Header_h
+#define __0_Header_h
+
+void unuionL (List *La,List Lb);
+
+
+#endif
--- 1-0/1-0/main.c
+++ 1-0/1-0/main.c
@@ -10,7 +10,15 @@
 
 int main(int argc, const char * argv[])
 {
-
+	char firststring[100]="Hello, World!!";
+	int i=0;
+	while (firststring[i]!='\0')
+	{
+			i++;
+	}
+	printf("%s, %d\n",firststring,i);
+	
+	
 	// insert code here...
 	printf("Hello, World!\n");
     return 0;
