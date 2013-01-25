5f058acc4ecaf3cdada4bee1c273f266a975549a
jim <kkmetsky@hotmail.com>
2013-01-25 13:53:05 +0800

    [function]Double_link_list

---
+++ Link_line/Link_line/Double_linked_list/Double_linked_list/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Double_linked_list
+//
+//  Created by liu yao-chun on 13/1/25.
+//  Copyright (c) 2013年 liu yao-chun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
